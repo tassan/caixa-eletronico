92e016baf373914a9f6cda409b6bc2e8933cf488
Flávio Tassan <ftassan@outlook.com>
2023-06-06 16:38:00 -0300

    finalizando estrutura base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react"
 import Depositar from "./components/Depositar"
 import Extrato from "./components/Extrato"
 import Sacar from "./components/Sacar"
 import Saldo from "./components/Saldo"
 import { CommandsContainer, Container, Title } from "./styles"
 
+
 function App() {
-  const [saldo, setSaldo] = useState(0);
-
-
   return (
     <Container>
       <Title>Caixa Eletrônico</Title>
       <iframe src="https://giphy.com/embed/9rwJNLBu8FMfixoIxL" width="480" height="270" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/andersonpaak-money-bills-9rwJNLBu8FMfixoIxL">via GIPHY</a></p>
-      <Saldo saldo={saldo} />
+      <Saldo saldo={0} />
       <CommandsContainer>
         <Depositar />
         <Sacar />
